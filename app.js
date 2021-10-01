8ce2e5cb8e0ee7aeff74b7854be91d2a612ceadc
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-10-01 18:41:24 +0530

    feat: add api request handle

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ var textInput = document.querySelector("#text-input");
 var buttonTranslate = document.querySelector("#button-translate");
 var textOutput = document.querySelector("#text-output");
 
+var apiURL ="https://api.funtranslations.com/translate/yoda.json";
+
+function getURL(text){
+    return apiURL + "?text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Error:", error);
+}
+
 function buttonClickEventHandler(){
+
+    const inputValue = textInput.value;
+
+    fetch(getURL(inputValue))
+    .then((res)=> res.json())
+    .then((json)=>{
+        const translatedText = json.contents.translated;
+        textOutput.innerHTML = translatedText;
+    }).catch(errorHandler);
     
-    textOutput.innerText = textInput.value;
 }
 
 buttonTranslate.addEventListener("click",buttonClickEventHandler);
