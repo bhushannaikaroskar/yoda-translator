2d2cd4d3f6b71e908c2c0d37a47b5311da59bd3e
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-10-02 04:09:48 +0530

    fix: add error handler

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ function getURL(text){
 
 function errorHandler(error){
     console.log("Error:", error);
+    if(error == 429){
+        alert("Request Limit reached. Only 5 requests allowed per hour");
+    }else{
+        alert("Something went wrong. Please try again later");
+    }
 }
 
 function buttonClickEventHandler(){
@@ -18,7 +23,14 @@ function buttonClickEventHandler(){
     const inputValue = textInput.value;
 
     fetch(getURL(inputValue))
-    .then((res)=> res.json())
+    .then((res)=> {
+        var result = res.json();
+        if(res.status == 200){
+            return result;
+        }else{
+            throw 429;
+        }  
+    })
     .then((json)=>{
         const translatedText = json.contents.translated;
         textOutput.innerHTML = translatedText;
