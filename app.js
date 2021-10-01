8053c37cd30a58dfc09e4e3bc30eab0a270e7ec3
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-10-01 16:59:01 +0530

    feat: add input output and button function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("File testing");
+
+var textInput = document.querySelector("#text-input");
+var buttonTranslate = document.querySelector("#button-translate");
+var textOutput = document.querySelector("#text-output");
+
+function buttonClickEventHandler(){
+    
+    textOutput.innerText = textInput.value;
+}
+
+buttonTranslate.addEventListener("click",buttonClickEventHandler);
